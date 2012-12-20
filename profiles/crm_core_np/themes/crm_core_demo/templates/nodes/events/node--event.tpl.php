08a1ada9eabee66a9de678b9cb5d20a6a22acded
Michael Haggerty <mhaggerty@trellon.com>
2012-12-20 15:05:44 -0500

    modifications to event registration

--- profiles/crm_core_np/themes/crm_core_demo/templates/nodes/events/node--event.tpl.php
+++ profiles/crm_core_np/themes/crm_core_demo/templates/nodes/events/node--event.tpl.php
@@ -8,26 +8,43 @@
     <?php print render($title_suffix); ?>
   </header>
 
-  <?php
-    // Hide comments, tags, and links now so that we can render them later.
-    hide($content['crm_core_event_reg_form']);
-    hide($content['comments']);
-    hide($content['links']);
-    hide($content['field_tags']);
-    print render($content);
-  ?>
-
+  <div class="row-fluid">
+  	<div class="span7">
+  	
+  		<div class="row-fluid">
+      	<div class="span6"><strong>When:</strong></div>
+      	<div class="span6"><strong>Where:</strong></div>
+  		</div>
+  	
+  		<div class="row-fluid">
+      	<div class="span6"><strong>Registration:</strong></div>
+      	<div class="span6"><strong>Contact:</strong></div>
+  		</div>
+      <?php
+        // Hide comments, tags, and links now so that we can render them later.
+        hide($content['crm_core_event_reg_form']);
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['field_tags']);
+        print render($content);
+      ?>
+  	</div>
+  	<div class="span5">
+    	<div class="well well-large">
+  	    <h3>Register</h3>
+  		  <?php print render($content['crm_core_event_reg_form']); ?>
+    	</div>
+  	</div>
+  </div>
+  
+  
   <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
     <footer>
       <?php print render($content['field_tags']); ?>
       <?php print render($content['links']); ?>
     </footer>
   <?php endif; ?>
-	
-	<div class="well well-large">
-    <h3>Register</h3>
-	  <?php print render($content['crm_core_event_reg_form']); ?>
-  </div>
+  
   
           
 
