a83bc92fba2df55e452d0cc3498cde822a94b78e
Michael Haggerty <mhaggerty@trellon.com>
2012-12-21 01:41:58 -0500

    theme enhancements - placing icons in place of textformat descriptions and the like. making things a little ugly on the way to beautiful

--- profiles/crm_core_np/themes/crm_core_demo/scripts/crm_core_demo.js
+++ profiles/crm_core_np/themes/crm_core_demo/scripts/crm_core_demo.js
@@ -8,23 +8,58 @@
         html: true,
       };
       
+      // apply tooltips to tips for html formats
       $('.tips', context).each(function(idx, val){
         var item = $(this);
         options.title = item.html();
         item.tooltip(options);
       });
       
+      // we need these tips to apply selectively, not throughout the entire interface
+      // maybe limit them to content entry forms?
+      
       // do the same with descriptions for various fields
       $('.text-format-wrapper > .description', context).each(function(idx, val){
-        var item = $(this);
+        var item = $(this),
+        label = item.parent().children('.control-group').children('LABEL');
+        
         options.title = item.html();
         item.tooltip(options);
+        
+        // position the tip just to the right of the label
+        item.css('left', label.width() + 20);
+        
+      });
+      
+      // do the same with textfields
+      $('.form-type-textfield > .controls > .help-block', context).each(function(idx, val){
+        var item = $(this), 
+        label = item.parents('.control-group').children('LABEL');
+        options.title = item.html();
+        item.tooltip(options);
+        
+        // position the tip just to the right of the label
+        // console.log(label);
+        item.css('left', label.width() + 20);
+        
+      });
+      
+      // do the same with image upload fields
+      
+      // 
+      $('.form-type-managed-file > .controls > .help-block', context).each(function(idx, val){
+        var item = $(this), 
+        label = item.parents('.control-group').children('LABEL');
+        options.title = item.html();
+        item.tooltip(options);
+        
+        // position the tip just to the right of the label
+        // console.log(label);
+        item.css('left', label.width() + 20);
+        
       });
       
       // set up all textareas to autosize
-      // $('textarea', context).autosize();
-      
-      
       $('textarea', context).each(function(idx, val){
         var item = $(this);
         console.log('found one');
