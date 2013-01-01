dd2239e3338eff9c3d2bd60d1d44e85e5016f390
Michael Haggerty <mhaggerty@trellon.com>
2013-01-01 17:55:13 -0500

    rearranging css files in the theme

---
+++ profiles/crm_core_np/themes/crm_core_demo/templates/views/petition_totals/views-view-unformatted--petition-totals--default.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($rows as $id => $row): ?>
+  <div class="row-fluid">
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
