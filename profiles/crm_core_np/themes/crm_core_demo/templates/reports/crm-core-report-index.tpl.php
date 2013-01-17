81830d217529fcad674a8c84f81b512c61d97948
Michael Haggerty <mhaggerty@trellon.com>
2013-01-17 08:50:04 -0500

    adding some styling to the reports page

--- profiles/crm_core_np/themes/crm_core_demo/templates/reports/crm-core-report-index.tpl.php
+++ profiles/crm_core_np/themes/crm_core_demo/templates/reports/crm-core-report-index.tpl.php
@@ -17,15 +17,56 @@
  *   - widgets: A list of widgets indexed by CRM Core.
  */
 
-  // dpm(get_defined_vars());
-  // dpm($reports);
-  // dpm($variables['reports']);
-
+  // dressing the reports page up to make it fancier
+  // by separating reports out into two columns.
+  // we are setting some important reports to appear at the top of each column,
+  // then displaying the rest of the reports below. This should be in a preprocessing
+  // function, but we are doing it here so people can see what is going on.
+  
+	  $donations = $reports['donations'];
+    $donations = theme('crm_core_report_index_item', array('title' => $donations['title'], 'report' => $donations['reports']));
+	  unset($reports['donations']);
+  
+	  $volunteer = $reports['volunteer'];
+    $volunteer = theme('crm_core_report_index_item', array('title' => $volunteer['title'], 'report' => $volunteer['reports']));
+	  unset($reports['volunteer']);
+	  
+	  $first = array();
+	  $second = array();
+	  
+	  $counter = sizeof($reports);
+	  
+	  foreach($reports as $item => $val){
+	    if($counter%3){
+	      $first[$item] = $val; 
+	    } else {
+	      $second[$item] = $val; 
+	    }
+	    $counter--;
+	  }
 ?>
 <div class="crm_core_reports">
-	<?php 
-	  foreach($reports as $item => $listing){
-	    print theme('crm_core_report_index_item', array('title' => $listing['title'], 'report' => $listing['reports']));
-	  }
-	?>
+	<div class="row-fluid">
+		<div class="span6">
+		
+			<?php 
+			  print $donations;
+    	  foreach($first as $item => $listing){
+    	    print theme('crm_core_report_index_item', array('title' => $first[$item]['title'], 'report' => $first[$item]['reports']));
+    	  }
+			?>
+		
+		</div>
+		<div class="span6">
+			<?php 
+			  print $volunteer;
+    	  foreach($second as $item => $listing){
+    	    print theme('crm_core_report_index_item', array('title' => $second[$item]['title'], 'report' => $second[$item]['reports']));
+    	  }
+			?>
+		
+		</div>
+	
+	</div>
+
 </div>
